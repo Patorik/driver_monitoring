f8324758d9b9ee0f898ab576f4f71bf42a07c295
Patrik Bezi <p4t0r1k@yahoo.com>
2023-08-25 10:03:13 +0200

    Created analyzer node functions

--- src/eye_analyser/eye_analyser/eye_analyser.py
+++ src/eye_analyser/eye_analyser/eye_analyser.py
@@ -1,6 +1,42 @@
-def main():
-    print('Hi from eye_analyser.')
+import rclpy
+from rclpy.node import Node
 
+from std_msgs.msg import Bool
 
-if __name__ == '__main__':
-    main()
+class Analyser(Node):
+    def __init__(self):
+        super().__init__('driver_detector')
+
+    def analyze(self):
+        pass
+
+    def detectBlink(self):
+        """
+        Detects if the person is blinking. 
+        """
+        
+    def estimateGazeDirection(self):
+        """
+        Estimate where the analyzed person is looking at.
+        """
+
+    def detectDisturbedEye(self):
+        """
+        Detects if an eye is disturbed based on blinking time.
+        """
+
+    def detectSleep(self):
+        """
+        Detects if the person is sleeping based on blinking time.
+        """
+
+    def distanceBetweenPoints(self, point_a, point_b):
+        """
+        Returns Euclidean distance between two points.
+        """
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    analyser = Analyser()
+    analyser.analyze()
